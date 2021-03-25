fc1be8e8cec110c2017754ba08ab2c1f18dbca18
Erik Huang <ekhu94@gmail.com>
2021-03-25 15:15:40 -0700

    finish relationships for serializers

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :cart_id
+  belongs_to :cart
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email_address
+  has_one :cart
 end
