77996197dd532acb7fcb88dbc7128462f0d84ac3
oopanpan <oopanpan@gmail.com>
2021-03-25 23:12:44 -0700

    serializer relationship edit

--- app/serializers/inventory_serializer.rb
+++ app/serializers/inventory_serializer.rb
@@ -1,3 +1,4 @@
 class InventorySerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :mens, :front_URL, :back_URL
+  has_many :items
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :quantity
-  has_one :cart
-  has_one :inventory
+  belongs_to :cart
+  belongs_to :inventory
 end
