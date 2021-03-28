d08e469660ca6a7806afda62bb1a7e52ffb22e4e
Erik Huang <ekhu94@gmail.com>
2021-03-28 13:23:38 -0700

    pull from pan's branch

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::AuthController < ApplicationController
 
     def create
         #! login action
-        @user = User.find_by(email_address: user_login_params[:email_address])
-        if @user && @user.authenticate(user_login_params[:password])
-            token = encode_token({ user_id: @user.id })
+        user = User.find_by(email_address: user_login_params[:email_address])
+        if user && user.authenticate(user_login_params[:password])
+            token = encode_token({ user_id: user.id })
             render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
         else
             render json: { message: 'Invalid email address or password' }, status: :unauthorized
@@ -15,6 +15,6 @@ class Api::V1::AuthController < ApplicationController
     private
 
     def user_login_params
-        params.require(:user).permit(:username, :email_address, :password, :password_confirmation)
+        params.require(:user).permit(:email_address, :password)
     end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,9 +7,9 @@ class Api::V1::UsersController < ApplicationController
 
     def create
         #! sign-up action
-        @user = User.create(user_params)
-        if @user.valid?
-            render json: { user: UserSerializer.new(@user) }, status: :created
+        user = User.create(user_params)
+        if user.valid?
+            render json: { user: UserSerializer.new(user) }, status: :created
         else
             render json: { error: 'failed to create user' }, status: :not_acceptable
         end
