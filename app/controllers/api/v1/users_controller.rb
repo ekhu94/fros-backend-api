ec9af75c37eefac861f450a22146fe0f4f1fdf2f
oopanpan <oopanpan@gmail.com>
2021-03-28 13:29:41 -0700

    exporting token from user#creat to front

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,9 +7,10 @@ class Api::V1::UsersController < ApplicationController
 
     def create
         #! sign-up action
-        @user = User.create(user_params)
-        if @user.valid?
-            render json: { user: UserSerializer.new(@user) }, status: :created
+        user = User.create(user_params)
+        if user.valid?
+            token = encode_token({user_id: user.id})
+            render json: { user: UserSerializer.new(user), jwt:token }, status: :created
         else
             render json: { error: 'failed to create user' }, status: :not_acceptable
         end
