d757ecaaf3c32d7b0dab64c8be104cd276f0a1da
Erik Huang <ekhu94@gmail.com>
2021-03-31 15:26:49 -0700

    test commit

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :carts
-          resources :users, only: [:create, :index]
+          resources :users, only: [:create, :index, :show]
       post '/login', to: 'auth#login'
       get '/auto_login', to: 'auth#auto_login' #* USE MAYBE?!
       get '/profile', to: 'users#profile'
