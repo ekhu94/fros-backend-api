b41567fceb48bda7d51cc5b728f78223b29424d6
Erik Huang <ekhu94@gmail.com>
2021-04-01 08:15:21 -0700

    add api/v1

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-    resource 'https://fros-api.herokuapp.com',
+    resource 'https://fros-api.herokuapp.com/api/v1/',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
