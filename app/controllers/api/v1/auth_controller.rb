65102ca8d7f017f5893bb16ef374eae9275f6c97
oopanpan <oopanpan@gmail.com>
2021-04-02 10:25:37 -0700

    serialized the user data

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::AuthController < ApplicationController
         user = User.find_by(email_address: params[:email_address])
         if user && user.authenticate(params[:password])
             token = encode_token({ user_id: user.id })
-            render json: { id: user.id, username: user.username, carts: user.carts, jwt: token }, status: :accepted
+            render json: { user: UserSerializer.new(user), jwt: token }, status: :accepted
         else
             render json: { message: 'Invalid email address or password' }, status: :unauthorized
         end
