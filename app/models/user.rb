271ba811ab0ac37803d0b475feda95b9625ba64d
Erik Huang <ekhu94@gmail.com>
2021-04-04 21:01:46 -0700

    very minor punctuation change on error msg

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_many :items, through: :carts
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
-    validates :email_address, presence: true, uniqueness: true, email_format: {message: 'is not valid'}
+    validates :email_address, presence: true, uniqueness: true, email_format: {message: 'is not valid. '}
 
     accepts_nested_attributes_for :carts
 end
